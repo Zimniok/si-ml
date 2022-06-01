ec495a1f0fc55bc05aa00548385a977dfa5bdf36
zimniok <pierzchala.ma@gmail.com>
2022-06-01 17:54:14 +0200

    WIP

--- data_reader.py
+++ data_reader.py
@@ -2,7 +2,17 @@ import csv
 import json
 import numpy as np
 
-GENRES_TO_FILTER = ['Satire', 'Roman', 'Science Fiction']
+GENRES_TO_FILTER = ['Science Fiction', 'Fantasy', 'Mystery', 'Historical novel']
+MY_GENRES = ['Fantasy', 'Mystery', 'Historical novel', 'Horror']
+
+CHARS_TO_REMOVE = '!?.,:;)('
+
+
+def get_data(genres_to_filter):
+    data = read_data()
+    output = filter_genres(data, genres_to_filter)
+    # print(output)
+    return to_x_y(output)
 
 
 def read_data():
@@ -15,8 +25,7 @@ def read_data():
                 genres = json.loads(row[5])
                 genres = list(genres.values())
             output.append([genres, row[6]])
-    output = filter_genres(output, GENRES_TO_FILTER)
-    return to_x_y(output)
+    return output
 
 
 def filter_genres(data, genres):
@@ -35,6 +44,42 @@ def to_x_y(data):
     x = []
     y = []
     for row in data:
-        x.append(row[1])
+        # x.append(row[1])
         y.append(row[0])
     return np.array(x), np.array(y)
+
+
+def analyze():
+    data = read_data()
+    output = {}
+    for row in data:
+        for genre in row[0]:
+            if genre in output:
+                output[genre] = output[genre] + 1
+            else:
+                output[genre] = 1
+    return dict(sorted(output.items(), key=lambda item: item[1], reverse=True))
+    # Fantasy Mystery Historical novel Horror
+
+
+def make_dict(filtered_data):
+    counts = {}  # word: (genres, books count)
+    for row in filtered_data:
+        dsc = row[1]
+        for char in CHARS_TO_REMOVE:
+            dsc = dsc.replace(char, '')
+        words = dsc.split(' ')
+        for word in words:
+            if word == '':
+                continue
+            word = word.lower()
+            if word in counts:
+                if row[0] not in counts[word][0]:
+                    counts[word][0].append(row[0])
+                counts[word] = counts[word][0], counts[word][1] + 1
+            else:
+                counts[word] = [row[0]], 1
+
+    counts = {k: v for k, v in counts.items() if len(v[0]) < 4}
+
+    print(dict(sorted(counts.items(), key=lambda item: item[1][1])))
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from data_reader import read_data
+import data_reader
 import numpy as np
 
 
 if __name__ == '__main__':
-    x, y = read_data()
+    x, y = data_reader.get_data(data_reader.GENRES_TO_FILTER)
     print(x)
     print(y)
 
+    unique, counts = np.unique(y, return_counts=True)
+    print(dict(zip(unique, counts)))
+
+    print('\n ANALYZE \n')
+    print(data_reader.analyze())
+
+    data_reader.make_dict(data_reader.filter_genres(data_reader.read_data(), data_reader.GENRES_TO_FILTER))
+
+    # print('\n MY GENRES \n')
+    #
+    # x2, y2 = data_reader.get_data(data_reader.MY_GENRES)
+    # print(x2)
+    # print(y2)
+    #
+    # unique, counts = np.unique(y2, return_counts=True)
+    # print(dict(zip(unique, counts)))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
